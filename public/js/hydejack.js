1c5d95ca2aa635bff2f66a1a74ed5ceac0a3eb3c
Florian Klampfer <f.klampfer@gmail.com>
2016-09-12 20:11:08 +0200

    Remove tap.js (deprecated)

--- public/js/hydejack.js
+++ public/js/hydejack.js
@@ -123,130 +123,6 @@
 }( this ));
 
 (function(document, window) {
-  /**
-   * tap.js
-   * Copyright (c) 2015 Alex Gibson
-   * https://github.com/alexgibson/tap.js/
-   * Released under MIT license
-   */
-  function Tap(el) {
-      this.el = typeof el === 'object' ? el : document.getElementById(el);
-      this.moved = false; //flags if the finger has moved
-      this.startX = 0; //starting x coordinate
-      this.startY = 0; //starting y coordinate
-      this.hasTouchEventOccured = false; //flag touch event
-      this.el.addEventListener('touchstart', this, false);
-      this.el.addEventListener('mousedown', this, false);
-  }
-
-  // return true if left click is in the event, handle many browsers
-  Tap.prototype.leftButton = function(event) {
-      // modern & preferred:  MSIE>=9, Firefox(all)
-      if ('buttons' in event) {
-         // https://developer.mozilla.org/docs/Web/API/MouseEvent/buttons
-         return event.buttons === 1;
-      } else {
-         return 'which' in event ?
-             // 'which' is well defined (and doesn't exist on MSIE<=8)
-             // https://developer.mozilla.org/docs/Web/API/MouseEvent/which
-             event.which === 1 :
-             // for MSIE<=8 button is 1=left (0 on all other browsers)
-             // https://developer.mozilla.org/docs/Web/API/MouseEvent/button
-             event.button === 1;
-      }
-      return false;
-  };
-
-  Tap.prototype.start = function(e) {
-      if (e.type === 'touchstart') {
-
-          this.hasTouchEventOccured = true;
-          this.el.addEventListener('touchmove', this, false);
-          this.el.addEventListener('touchend', this, false);
-          this.el.addEventListener('touchcancel', this, false);
-
-      } else if (e.type === 'mousedown' && this.leftButton(e)) {
-
-          this.el.addEventListener('mousemove', this, false);
-          this.el.addEventListener('mouseup', this, false);
-      }
-
-      this.moved = false;
-      this.startX = e.type === 'touchstart' ? e.touches[0].clientX : e.clientX;
-      this.startY = e.type === 'touchstart' ? e.touches[0].clientY : e.clientY;
-  };
-
-  Tap.prototype.move = function(e) {
-      //if finger moves more than 10px flag to cancel
-      var x = e.type === 'touchmove' ? e.touches[0].clientX : e.clientX;
-      var y = e.type === 'touchmove' ? e.touches[0].clientY : e.clientY;
-      if (Math.abs(x - this.startX) > 10 || Math.abs(y - this.startY) > 10) {
-          this.moved = true;
-      }
-  };
-
-  Tap.prototype.end = function(e) {
-      var evt;
-
-      this.el.removeEventListener('touchmove', this, false);
-      this.el.removeEventListener('touchend', this, false);
-      this.el.removeEventListener('touchcancel', this, false);
-      this.el.removeEventListener('mouseup', this, false);
-      this.el.removeEventListener('mousemove', this, false);
-
-      if (!this.moved) {
-          //create custom event
-          try {
-              evt = new window.CustomEvent('tap', {
-                  bubbles: true,
-                  cancelable: true
-              });
-          } catch (e) {
-              evt = document.createEvent('Event');
-              evt.initEvent('tap', true, true);
-          }
-
-          //prevent touchend from propagating to any parent
-          //nodes that may have a tap.js listener attached
-          e.stopPropagation();
-
-          // dispatchEvent returns false if any handler calls preventDefault,
-          if (!e.target.dispatchEvent(evt)) {
-              // in which case we want to prevent clicks from firing.
-              e.preventDefault();
-          }
-      }
-  };
-
-  Tap.prototype.cancel = function() {
-      this.hasTouchEventOccured = false;
-      this.moved = false;
-      this.startX = 0;
-      this.startY = 0;
-  };
-
-  Tap.prototype.destroy = function() {
-      this.el.removeEventListener('touchstart', this, false);
-      this.el.removeEventListener('touchmove', this, false);
-      this.el.removeEventListener('touchend', this, false);
-      this.el.removeEventListener('touchcancel', this, false);
-      this.el.removeEventListener('mousedown', this, false);
-      this.el.removeEventListener('mouseup', this, false);
-      this.el.removeEventListener('mousemove', this, false);
-  };
-
-  Tap.prototype.handleEvent = function(e) {
-      switch (e.type) {
-          case 'touchstart': this.start(e); break;
-          case 'touchmove': this.move(e); break;
-          case 'touchend': this.end(e); break;
-          case 'touchcancel': this.cancel(e); break;
-          case 'mousedown': this.start(e); break;
-          case 'mouseup': this.end(e); break;
-          case 'mousemove': this.move(e); break;
-      }
-  };
-
   function polyfillRequestAnimationFrame() {
     var lastTime = 0;
     var vendors = ['webkit', 'moz'];
@@ -310,9 +186,6 @@
    */
   function createSidebar(sidebar, backdrop, menu) {
     polyfillRequestAnimationFrame();
-
-    new Tap(backdrop);
-    new Tap(menu);
 
     var browerCapabilities = getBrowserCapabilities();
     // var transformPrefix = browerCapabilities.prefix;
@@ -558,16 +431,16 @@
     function enableSlider() {
       document.body.classList.add('drawer');
       document.addEventListener('touchstart', onTouchStart);
-      menu.addEventListener('tap', onMenuClick);
-      backdrop.addEventListener('tap', onBackdropClick);
+      menu.addEventListener('click', onMenuClick);
+      backdrop.addEventListener('click', onBackdropClick);
       animateTo(menuOpen);
     }
 
     function disableSlider() {
       document.body.classList.remove('drawer')
       document.removeEventListener('touchstart', onTouchStart);
-      menu.removeEventListener('tap', onMenuClick);
-      backdrop.removeEventListener('tap', onBackdropClick);
+      menu.removeEventListener('click', onMenuClick);
+      backdrop.removeEventListener('click', onBackdropClick);
     }
 
     function onResize() {
