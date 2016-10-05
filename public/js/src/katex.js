190772663f445f984e880bc2a9afa272f4de70fb
Florian Klampfer <f.klampfer@gmail.com>
2016-10-05 16:34:28 +0200

    wip

--- public/js/src/katex.js
+++ public/js/src/katex.js
@@ -3,42 +3,51 @@ import { loadCSS } from 'fg-loadcss/src/loadCSS';
 
 import hasFeatures from '../lib/has-features';
 
-export default function asdf() {
+export default function upgradeMathBlocks() {
   /* global katex */
   if (!window.katex) return;
 
   const mathBlocks = document.querySelectorAll('script[type^="math/tex"]');
 
-  // kramdown generates script tags with type "math/tex"
-  for (const element of mathBlocks) {
-    const el = element;
-
-    const tex = el.textContent
-      .replace('% <![CDATA[', '')
-      .replace('%]]>', '');
-
-    // replace the script tag with KaTeX
-    try {
-      const preview = el.previousElementSibling;
-
-      el.outerHTML = katex.renderToString(tex, {
-        displayMode: el.type === 'math/tex; mode=display',
-      });
-
-      // hide the preview only when successful
-      preview.style.display = 'none';
-      preview.style.visibility = 'hidden';
-    } catch (e) {
-      console.error(e); // eslint-disable-line no-console
-    }
+  for (const mathBlock of mathBlocks) {
+    mathBlock.style.willChange = 'content';
   }
+
+  requestAnimationFrame(() => {
+    // kramdown generates script tags with type "math/tex"
+    for (const element of mathBlocks) {
+      const el = element;
+
+      const tex = el.textContent
+        .replace('% <![CDATA[', '')
+        .replace('%]]>', '');
+
+      // replace the script tag with KaTeX
+      try {
+        const preview = el.previousElementSibling;
+
+        el.outerHTML = katex.renderToString(tex, {
+          displayMode: el.type === 'math/tex; mode=display',
+        });
+
+        // hide the preview only when successful
+        preview.style.display = 'none';
+        preview.style.visibility = 'hidden';
+
+        el.style.willChange = '';
+      } catch (e) {
+        console.error(e); // eslint-disable-line no-console
+      }
+    }
+  });
 }
 
 // KaTeX support
 if (hasFeatures(['queryselector',
                  'classlist',
+                 'requestanimationframe',
                ])) {
   // enable math blocks using KaTeX
   loadCSS('https://unpkg.com/katex@0.6.0/dist/katex.min.css');
-  loadJSDeferred('https://unpkg.com/katex@0.6.0/dist/katex.min.js', asdf);
+  loadJSDeferred('https://unpkg.com/katex@0.6.0/dist/katex.min.js', upgradeMathBlocks);
 }
--- public/js/src/smooth-state.js
+++ public/js/src/smooth-state.js
@@ -1,24 +1,32 @@
-import SmoothState from './smooth-state-lib';
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+import SmoothState from 'y-smooth-state/src/vanilla';
 
 import upgradeKatex from './katex';
 
-const smoothState = document.querySelector('#_smooth-state');
+const smoothState = document.querySelector('y-smooth-state');
+
+window.setImmediate = window.setImmediate || (f => window.setTimeout(f, 0));
 
 window.smoothState = new SmoothState(smoothState, {
   // replaceIds: ['_main', '_asidebar'],
   contentSelector: 'main, header > div',
+  scroll: true,
 });
 
-smoothState.addEventListener('beforesmoothstate', () => {
-  if (!window.isDesktop && window.drawer.opened) {
-    window.drawer.close();
-  }
+smoothState.addEventListener('y-smooth-state-before', () => {
+  setImmediate(() => {
+    if (!window.isDesktop && window.drawer.opened) {
+      window.drawer.close();
+    }
+  });
 });
 
-smoothState.addEventListener('aftersmoothstate', () => {
-   // send google analytics pageview
-  if (window.ga) window.ga('send', 'pageview');
+smoothState.addEventListener('y-smooth-state-after', () => {
+  setImmediate(() => {
+    // send google analytics pageview
+    if (window.ga) window.ga('send', 'pageview');
 
-  // upgrade math blocks
-  upgradeKatex();
+    // upgrade math blocks
+    upgradeKatex();
+  });
 });
