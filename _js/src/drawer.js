5da4ed4bdcbcf29c2b7466b9597f626395b67d63
Florian Klampfer <f.klampfer@gmail.com>
2016-10-28 12:07:59 +0200

    Refactor

---
+++ _js/src/drawer.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import { loadCSS } from 'fg-loadcss/src/loadCSS';
+import YDrawer from 'y-drawer/src/vanilla';
+// import HTMLYDrawerElement from 'y-drawer/src/webcomponent';
+
+import { hasFeatures /* , defineCustomElement, ensureCustomElements */ } from './common';
+
+const REQUIREMENTS = [
+  'eventlistener',
+  'queryselector',
+  'matchmedia',
+  'requestanimationframe',
+  'classlist',
+  'opacity',
+  'csstransforms',
+  'csspointerevents',
+  'cssremunit',
+];
+
+const MEDIA_QUERY = '(min-width: 50em)';
+
+function resizeCallback() {
+  const hasChanged = window.isDesktop !== window.matchMedia(MEDIA_QUERY).matches;
+  if (hasChanged) {
+    window.isDesktop = !window.isDesktop;
+    window.drawer.persistent = window.isDesktop;
+    window.drawer.jumpTo(window.isDesktop);
+  }
+}
+
+function menuClickClallback(e) {
+  if (!window.isDesktop) {
+    e.preventDefault();
+    window.drawer.toggle();
+  }
+}
+
+function addEventListeners(drawer) {
+  window.drawer = drawer;
+  window.addEventListener('resize', resizeCallback);
+  document.getElementById('_menu').addEventListener('click', menuClickClallback);
+}
+
+// function defineYDrawer() {
+//   defineCustomElement('y-drawer', HTMLYDrawerElement);
+//   addEventListeners(document.querySelector('y-drawer'));
+// }
+
+// function withShadowDOM(drawer) {
+//   if (window.isDesktop) drawer.setAttribute('opened', '');
+//   if (window.isDesktop) drawer.setAttribute('persistent', '');
+//   ensureCustomElements(defineYDrawer);
+// }
+
+function globalCSSLoaded(drawer) {
+  // You Only Load Once
+  if (!(window.drawer instanceof YDrawer)) {
+    addEventListeners(new YDrawer(drawer, {
+      opened: window.isDesktop,
+      persistent: window.isDesktop,
+    }));
+  }
+}
+
+function withoutShadowDOM(drawer) {
+  const ref = document.getElementsByTagName('style')[0];
+  loadCSS('https://unpkg.com/y-drawer@3/dist/drawer.css', ref)
+    .addEventListener('load', () => globalCSSLoaded(drawer));
+}
+
+if (hasFeatures(REQUIREMENTS)) {
+  window.isDesktop = window.matchMedia(MEDIA_QUERY).matches;
+
+  const drawer = document.querySelector('y-drawer');
+
+  // if ('attachShadow' in document.body || 'createShadowRoot' in document.body) {
+  //   withShadowDOM(drawer);
+  // } else {
+  withoutShadowDOM(drawer);
+  // }
+}
---
+++ _js/src/push-state.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import { Observable } from 'rxjs-es/Observable';
+import 'rxjs-es/add/operator/debounceTime';
+import 'rxjs-es/add/operator/do';
+import 'rxjs-es/add/observable/fromEvent';
+
+import PushState from 'y-push-state/src/vanilla';
+// import HTMLPushStateElement from 'y-push-state/src/webcomponent';
+
+import { hasFeatures /* , defineCustomElement */ } from './common';
+import upgradeMathBlocks from './katex';
+
+const REQUIREMENTS = [
+  'eventlistener',
+  'queryselector',
+  'requestanimationframe',
+  'classlist',
+  'documentfragment',
+  'history',
+];
+
+const pushState = document.querySelector('y-push-state');
+
+function beforeCallback() {
+  document.body.classList.add('is-loading');
+
+  // unshow(error);
+
+  // if a link on the drawer has been clicked, close it
+  if (!window.isDesktop && window.drawer.opened) {
+    window.drawer.close();
+  }
+}
+
+function afterSequence($) {
+  return $
+    .do(() => document.body.classList.remove('is-loading'))
+    .debounceTime(200)
+    .subscribe(() => {
+      // send google analytics pageview
+      if (window.ga) window.ga('send', 'pageview');
+
+      // upgrade math blocks
+      upgradeMathBlocks();
+    });
+}
+
+function errorCallback() {
+  // TODO
+  // show(error);
+}
+
+// function withCustomElements() {
+//   const setup = () => {
+//     pushState.startHistory();
+//     pushState.removeEventListener('y-push-state-attached', setup);
+//   };
+//
+//   pushState.addEventListener('y-push-state-attached', setup);
+//   defineCustomElement('y-push-state', HTMLPushStateElement);
+// }
+
+function withoutCustomElements() {
+  new PushState(pushState, {
+    replaceIds: ['_main', '_asidebar'],
+    linkSelector: 'a[href^="/"]',
+    scrollRestoration: true,
+  }).startHistory();
+}
+
+if (hasFeatures(REQUIREMENTS)) {
+  pushState.addEventListener('y-push-state-before', beforeCallback);
+  pushState.addEventListener('y-smooth-state-error', errorCallback);
+  afterSequence(Observable.fromEvent(pushState, 'y-push-state-after'));
+
+  // if ('customElement' in window || 'registerElement' in document) {
+  //   withCustomElements();
+  // } else {
+  withoutCustomElements();
+  // }
+}
