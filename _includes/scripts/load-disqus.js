0f5c48abf6d090c21a2e3cf7f2f0bc2338f27cc5
Florian Klampfer <mail@qwtel.com>
2017-09-18 13:03:18 +0200

    update comments

--- _includes/scripts/load-disqus.js
+++ _includes/scripts/load-disqus.js
@@ -30,7 +30,7 @@
   if (!w._disqusLoading) {
     w._disqusLoading = true;
 
-    loadJSDeferred('//{{ site.disqus_shortname }}.disqus.com/embed.js');
+    loadJSDeferred('//{{ include.disqus }}.disqus.com/embed.js');
 
     // add event listener
     if (w.addEventListener) w.addEventListener('scroll', loadDQ, { passive: true });
