7eaf4ab4805bded0b15d151f05802434f26597c2
Florian Klampfer <mail@qwtel.com>
2017-12-13 09:45:30 +0100

    fix #53

--- _includes/scripts/load-disqus.js
+++ _includes/scripts/load-disqus.js
@@ -16,12 +16,6 @@
 // Compress via uglify:
 // uglifyjs load-disqus.js -c -m > load-disqus.min.js
 (function(w, d) {
-  var disqus_config = function () {
-    this.page.title = '{{ page.title }}';
-    this.page.identifier = '{{ page.id }}';
-    this.page.url = '{{ page.url | absolute_url }}';
-  };
-
   w._disqusFirst = typeof w._disqusFirst != 'undefined' ? w._disqusFirst : true;
   w._disqusLoading = typeof w._disqusLoading != 'undefined' ? w._disqusLoading : false;
   w._disqusThis = false;
@@ -29,18 +23,18 @@
 
   function loadDQ(e) {
     var scrollTop = w.pageYOffset || d.body.scrollTop;
-    if ( w.DISQUS &&
-        !w._disqusThis &&
-        !w._disqusFirst &&
+    if (w.DISQUS && !w._disqusThis && !w._disqusFirst &&
         scrollTop + w.innerHeight >= w._disqusThreadOffsetTop) {
-
       w._disqusThis = true;
       w.DISQUS.reset({
         reload: true,
-        config: disqus_config
+        config: function () {
+          this.page.url = window.location.href;
+          this.page.title = document.title;
+        },
       });
     }
-  };
+  }
 
   if (!w._disqusLoading) {
     w._disqusLoading = true;
