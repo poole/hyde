f614f56c200d9fcd0ffac203db257d6ca24cf724
Florian Klampfer <mail@qwtel.com>
2018-03-12 10:42:50 +0000

    update copyright

--- _js/src/flip/title.js
+++ _js/src/flip/title.js
@@ -1,5 +1,5 @@
 // # src / flip / title.js
-// Copyright (c) 2017 Florian Klampfer <https://qwtel.com/>
+// Copyright (c) 2018 Florian Klampfer <https://qwtel.com/>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
