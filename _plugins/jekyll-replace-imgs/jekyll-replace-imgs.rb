6d2f3bc603f6431ca0a4d1e0feae3da953170d84
Florian Klampfer <mail@qwtel.com>
2018-04-05 16:41:31 +0200

    improve relace imgs plugin

--- _plugins/jekyll-replace-imgs/jekyll-replace-imgs.rb
+++ _plugins/jekyll-replace-imgs/jekyll-replace-imgs.rb
@@ -1,7 +1,25 @@
-IMG_REGEX = /<img(.*?)\/>/m
+RE_IMG     = /<img(.*?)\/>/m
+RE_DATAURL = /.*src\s*=\s*["']\s*data:.*["']/
 
-REPLACE_STR = '<shy-img><noscript><img\1/></noscript></shy-img>'
+REPLACEMENT = '<noscript data-i="%{i}"><img%{attrs}/></noscript>'
 
-Jekyll::Hooks.register([:pages, :posts, :documents], :post_render) do |post|
-  post.output = post.output.gsub(IMG_REGEX, REPLACE_STR)
+CONFIG_KEY = 'replace_imgs'
+REPLACEMENT_KEY = 'replacement'
+
+Jekyll::Hooks.register([:pages, :documents], :post_render) do |page|
+  config ||= page.site.config
+  replacement ||= (config[CONFIG_KEY] && config[CONFIG_KEY][REPLACEMENT_KEY]) ||
+    REPLACEMENT
+
+  i = 0
+  page.output = page.output.gsub(RE_IMG) do |orig|
+    attrs = Regexp.last_match[1]
+
+    if orig.index(RE_DATAURL).nil? then
+      i += 1
+      replacement % { i:i, attrs:attrs }
+    else
+      orig
+    end
+  end
 end
