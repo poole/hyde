a7ae9cf300f9c187292ab7ea409cc0cd9c71b532
Florian Klampfer <mail@qwtel.com>
2018-04-06 13:43:39 +0200

    add license and doc to plugin

--- _plugins/jekyll-replace-imgs/jekyll-replace-imgs.rb
+++ _plugins/jekyll-replace-imgs/jekyll-replace-imgs.rb
@@ -1,3 +1,25 @@
+# Copyright (c) 2018 Florian Klampfer <https://qwtel.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is a tiny, optional plugin (will be ignored on GitHub Pages)
+# that replaces *all* <img> tags with placeholders that won't load immediately.
+# This can increase page load speed, especially for documents with lots of images.
+#
+# This requires a JS component to lazy-load the images.
+# See also: https://github.com/qwtel/shy-img
+
 RE_IMG     = /<img(.*?)\/>/m
 RE_DATAURL = /.*src\s*=\s*["']\s*data:.*["']/
 
@@ -12,14 +34,16 @@ Jekyll::Hooks.register([:pages, :documents], :post_render) do |page|
     REPLACEMENT
 
   i = 0
-  page.output = page.output.gsub(RE_IMG) do |orig|
+  page.output = page.output.gsub(RE_IMG) do |match|
     attrs = Regexp.last_match[1]
 
-    if orig.index(RE_DATAURL).nil? then
+    # TODO: run more logic here? parse attributes!?
+
+    if match.index(RE_DATAURL).nil? then
       i += 1
       replacement % { i:i, attrs:attrs }
     else
-      orig
+      match
     end
   end
 end
