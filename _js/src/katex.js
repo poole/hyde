eee7a67d0ec7532c80de3917deaf32b52eb6fc08
Florian Klampfer <mail@qwtel.com>
2019-07-29 15:15:47 +0700

    Remove font face observer in favor of font-display

--- _js/src/katex.js
+++ _js/src/katex.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { hasFeatures, hide } from "./common";
+import { hasFeatures } from "./common";
 
 const REQUIREMENTS = ["classlist", "eventlistener", "queryselector"];
 
