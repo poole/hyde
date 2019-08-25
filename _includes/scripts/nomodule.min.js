882d311eaa5b9fa5550c0e4c6c0637fb9e3386be
Florian Klampfer <mail@qwtel.com>
2019-08-25 13:38:51 +0700

    Better nomodule script loading

---
+++ _includes/scripts/nomodule.min.js
@@ -0,0 +1,1 @@
+!function(){var e=document.createElement("script");if(!("noModule"in e)&&"onbeforeload"in e){var t=!1;document.addEventListener("beforeload",function(n){if(n.target===e)t=!0;else if(!n.target.hasAttribute("nomodule")||!t)return;n.preventDefault()},!0),e.type="module",e.src=".",document.head.appendChild(e),e.remove()}}();
