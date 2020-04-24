1d89a1e78122d5148e6135133b5e919c3b4aa355
Florian Klampfer <mail@qwtel.com>
2020-04-24 13:54:05 +0700

    Update math

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ source "https://rubygems.org"
 # Happy Jekylling!
 gem "jekyll", "~> 4.0"
 
+gem "kramdown-math-katex"
+
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
 # gem "github-pages", group: :jekyll_plugins
--- _js/src/katex.js
+++ _js/src/katex.js
@@ -16,39 +16,19 @@
 if (!window._noKaTeX) {
   let loaded;
 
-  function renderKatex(el) {
-    try {
-      let prev = el.previousElementSibling;
-      while (prev && !prev.classList.contains('MathJax_Preview')) prev = prev.previousElementSibling;
-
-      const tex = el.textContent.replace('% <![CDATA[', '').replace('%]]>', '');
-
-      el.outerHTML = window.katex.renderToString(tex, {
-        displayMode: el.type === 'math/tex; mode=display',
-      });
-
-      if (prev) prev.parentNode.removeChild(prev);
-    } catch (e) {
-      if (process.env.DEBUG) console.error(e);
-    }
-  }
-
   const promisify = (load, href) => new Promise(resolve => load(href).addEventListener('load', resolve));
+  const loadJS = (href) => promisify(window.loadJS, href);
+  const loadCSS = (href) => promisify(window.loadCSS, href);
 
   const upgradeMathBlocks = () => {
-    const mathBlocks = document.querySelectorAll('script[type^="math/tex"]');
-    if (mathBlocks.length) {
-      if (!loaded) {
-        loaded = Promise.all([
-          promisify(loadJS, document.getElementById('_hrefKatexJS').href),
-          promisify(loadCSS, document.getElementById('_hrefKatexCSS').href),
-          promisify(loadJS, document.getElementById('_hrefKatexCopyJS').href),
-          promisify(loadCSS, document.getElementById('_hrefKatexCopyCSS').href),
-        ]);
-      }
-      loaded.then(() => {
-        Array.from(mathBlocks).forEach(renderKatex);
-      });
+    const mathBlocks = document.querySelectorAll('.katex');
+    if (mathBlocks.length && !loaded) {
+      loaded = Promise.all([
+        loadJS(document.getElementById('_hrefKatexJS').href),
+        loadCSS(document.getElementById('_hrefKatexCSS').href),
+        loadJS(document.getElementById('_hrefKatexCopyJS').href),
+        loadCSS(document.getElementById('_hrefKatexCopyCSS').href),
+      ]);
     }
   };
 
