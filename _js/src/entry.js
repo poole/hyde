4bf201edd880d86f775cfc1277e66862254ce3f3
Florian Klampfer <mail@qwtel.com>
2020-05-23 16:57:44 +0700

    Fix webpack loading for sites with base url

--- _js/src/entry.js
+++ _js/src/entry.js
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+__webpack_public_path__ = window._baseURL;
+
 import '@babel/polyfill';
 
 import '../lib/version';
