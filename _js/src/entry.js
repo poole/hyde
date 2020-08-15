7922b9cc85122c9e3baa1326d986baa9c6674884
Florian Klampfer <mail@qwtel.com>
2020-08-15 14:54:24 +0700

    Add clap button

--- _js/src/entry.js
+++ _js/src/entry.js
@@ -25,6 +25,7 @@ const BASELINE = ['classlist', 'eventlistener', 'queryselector', 'template'];
 const DARK_MODE_FEATURES = ['customproperties'];
 const DRAWER_FEATURES = ['customproperties', 'history', 'matchmedia', 'opacity'];
 const PUSH_STATE_FEATURES = ['history', 'matchmedia', 'opacity', 'cssanimations', 'cssremunit', 'documentfragment'];
+const CLAP_BUTTON_FEATURES = ['customproperties', 'cssanimations', 'cssremunit'];
 const TOC_FEATURES = ['matchmedia', 'cssremunit'];
 
 if (hasFeatures(BASELINE)) {
@@ -37,6 +38,10 @@ if (hasFeatures(BASELINE)) {
   //   import(/* webpackMode: "eager" */ './pro/cookies-banner');
   //   import(/* webpackMode: "eager" */ './pro/dark-mode');
   // }
+
+  if (window._clapButton && hasFeatures(CLAP_BUTTON_FEATURES)) {
+    import(/* webpackChunkName: "clap-button" */ './clap-button');
+  }
 
   // A list of Modernizr tests that are required for the drawer to work.
   if (!window._noDrawer && hasFeatures(DRAWER_FEATURES)) {
