38ee11f9e0732430baf876ce3bca88a3290509f5
Florian Klampfer <mail@qwtel.com>
2021-03-30 21:26:45 +0700

    9.1.4

---
+++ assets/js/LEGACY-clap-button-hydejack-9.1.4.js
@@ -0,0 +1,14 @@
+/*!
+ *  __  __                __                                     __
+ * /\ \/\ \              /\ \             __                    /\ \
+ * \ \ \_\ \   __  __    \_\ \      __   /\_\      __       ___ \ \ \/'\
+ *  \ \  _  \ /\ \/\ \   /'_` \   /'__`\ \/\ \   /'__`\    /'___\\ \ , <
+ *   \ \ \ \ \\ \ \_\ \ /\ \L\ \ /\  __/  \ \ \ /\ \L\.\_ /\ \__/ \ \ \\`\
+ *    \ \_\ \_\\/`____ \\ \___,_\\ \____\ _\ \ \\ \__/.\_\\ \____\ \ \_\ \_\
+ *     \/_/\/_/ `/___/> \\/__,_ / \/____//\ \_\ \\/__/\/_/ \/____/  \/_/\/_/
+ *                 /\___/                \ \____/
+ *                 \/__/                  \/___/
+ *
+ * Powered by Hydejack v9.1.4 <https://hydejack.com/>
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{303:function(n,r,e){"use strict";e.r(r);e(334);var t,o=e(22);function i(n){return function(n){if(Array.isArray(n))return a(n)}(n)||function(n){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(n))return Array.from(n)}(n)||function(n,r){if(!n)return;if("string"==typeof n)return a(n,r);var e=Object.prototype.toString.call(n).slice(8,-1);"Object"===e&&n.constructor&&(e=n.constructor.name);if("Map"===e||"Set"===e)return Array.from(n);if("Arguments"===e||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(e))return a(n,r)}(n)||function(){throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}()}function a(n,r){(null==r||r>n.length)&&(r=n.length);for(var e=0,t=new Array(r);e<r;e++)t[e]=n[e];return t}function u(n,r,e,t,o,i,a){try{var u=n[i](a),c=u.value}catch(n){return void e(n)}u.done?r(c):Promise.resolve(c).then(t,o)}(t=regeneratorRuntime.mark((function n(){return regeneratorRuntime.wrap((function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,Promise.all(i("customElements"in window?[]:[Promise.all([e.e(16),e.e(17)]).then(e.bind(null,328)).then((function(){return Promise.all([e.e(14),e.e(8)]).then(e.bind(null,329))}))]));case 2:return n.next=4,o.t;case 4:return n.next=6,o.s;case 6:window.GET_CLAPS_API||(window.GET_CLAPS_API="https://worker.getclaps.app"),Promise.resolve().then(e.t.bind(null,335,7));case 8:case"end":return n.stop()}}),n)})),function(){var n=this,r=arguments;return new Promise((function(e,o){var i=t.apply(n,r);function a(n){u(i,e,o,a,c,"next",n)}function c(n){u(i,e,o,a,c,"throw",n)}a(void 0)}))})()}}]);
---
+++ assets/js/LEGACY-webcomponents-hydejack-9.1.4.js
@@ -0,0 +1,14 @@
+/*!
+ *  __  __                __                                     __
+ * /\ \/\ \              /\ \             __                    /\ \
+ * \ \ \_\ \   __  __    \_\ \      __   /\_\      __       ___ \ \ \/'\
+ *  \ \  _  \ /\ \/\ \   /'_` \   /'__`\ \/\ \   /'__`\    /'___\\ \ , <
+ *   \ \ \ \ \\ \ \_\ \ /\ \L\ \ /\  __/  \ \ \ /\ \L\.\_ /\ \__/ \ \ \\`\
+ *    \ \_\ \_\\/`____ \\ \___,_\\ \____\ _\ \ \\ \__/.\_\\ \____\ \ \_\ \_\
+ *     \/_/\/_/ `/___/> \\/__,_ / \/____//\ \_\ \\/__/\/_/ \/____/  \/_/\/_/
+ *                 /\___/                \ \____/
+ *                 \/__/                  \/___/
+ *
+ * Powered by Hydejack v9.1.4 <https://hydejack.com/>
+ */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{328:function(e,n,t){"use strict";t.r(n);t(336),t(337),t(338),t(339);var o=window.customElements,d=!1,l=null;function a(){window.HTMLTemplateElement.bootstrap&&window.HTMLTemplateElement.bootstrap(window.document),l&&l(),d=!0,document.dispatchEvent(new CustomEvent("WebComponentsReady",{bubbles:!0}))}o.polyfillWrapFlushCallback&&o.polyfillWrapFlushCallback((function(e){l=e,d&&e()})),"complete"!==document.readyState?(window.addEventListener("load",a),window.addEventListener("DOMContentLoaded",(function(){window.removeEventListener("load",a),a()}))):a()}}]);
